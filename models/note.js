42cd5014514a9d5e64415f354cd17508e7d24598
Jim Fall <jimfall@gmail.com>
2014-09-25 11:07:43 -0700

    added changes to views

--- models/note.js
+++ models/note.js
@@ -3,7 +3,10 @@ var mongoose = require('mongoose');
 var noteSchema = mongoose.Schema({
   noteBody: String,
   name: String,
-  age: String
+  course: String,
+  rtg1: String,
+  rtg2: String,
+  rtg3: String
 });
 
 module.exports = mongoose.model('Note', noteSchema);
